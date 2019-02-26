69d6a3b393a14a58f406e75b3250a6bc492f94c7
sumi <smj11188@example.com>
2019-02-26 16:06:57 -0600

    added hello world program

---
+++ Hello.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Hello {
+
+	private static Integer number;
+
+	public static void main(String[] args) {
+		System.out.println("Helloe World");
+}
+}
